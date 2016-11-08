c30912dd22d6b172e935c02e79412e996b1beeef
Artur Grabowski <art@blahonga.org>
2016-11-08 15:55:34 +0100

    Shrinking the slice changes everything.
    Also, preallocate a backing array to remove that cost from the benchmarks. Now everything changes.

--- sl_test.go
+++ sl_test.go
@@ -2,7 +2,7 @@ package xxx_test
 
 import "testing"
 
-const sz = 300000000
+const sz = 200000000
 
 func BenchmarkInd(b *testing.B) {
 	for bc := 0; bc < b.N; bc++ {
@@ -59,6 +59,49 @@ func BenchmarkRArr(b *testing.B) {
 	for bc := 0; bc < b.N; bc++ {
 		x := [sz]int{}
 		for i := range x {
+			x[i] = i
+		}
+		b.SetBytes(sz)
+	}
+}
+
+var back = [sz]int{}
+
+func BenchmarkBInd(b *testing.B) {
+	for bc := 0; bc < b.N; bc++ {
+		x := back[:]
+		for i := 0; i < sz; i++ {
+			x[i] = i
+		}
+		b.SetBytes(sz)
+	}
+}
+
+func BenchmarkBRInd(b *testing.B) {
+	for bc := 0; bc < b.N; bc++ {
+		x := back[:]
+		for i := range x {
+			x[i] = i
+		}
+		b.SetBytes(sz)
+	}
+}
+
+func BenchmarkBApp(b *testing.B) {
+	for bc := 0; bc < b.N; bc++ {
+		x := back[:0]
+		for i := 0; i < sz; i++ {
+			x = append(x, i)
+		}
+		b.SetBytes(sz)
+	}
+}
+
+func BenchmarkBManual(b *testing.B) {
+	for bc := 0; bc < b.N; bc++ {
+		x := back[:0]
+		for i := 0; i < sz; i++ {
+			x = x[:i+1]
 			x[i] = i
 		}
 		b.SetBytes(sz)
